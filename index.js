6bc4790c8de36b3e9d81a4f31110578a090a6403
terence <terenceshawn18@outlook.com>
2023-10-02 05:55:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader=document.createElement('h1');
+newHeader.id='victory';
+
+newHeader.textContent='Terence is the champion'
